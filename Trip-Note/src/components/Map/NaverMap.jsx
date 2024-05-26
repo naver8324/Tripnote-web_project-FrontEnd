91421f1b1a5a6de96ca592f1cfcda3308071dc4f
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-05-26 11:21:57 +0900

    store확장자 변경

--- Trip-Note/src/components/Map/NaverMap.jsx
+++ Trip-Note/src/components/Map/NaverMap.jsx
@@ -24,19 +24,6 @@ export default function NaverMap({ className }) {
       document.head.removeChild(script);
     };
   }, []);
-  var map = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10,
-  });
-
-  var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.3595704, 127.105399),
-    map: map,
-  });
-
-  naver.maps.Event.addListener(map, 'click', function (e) {
-    marker.setPosition(e.latlng);
-  });
 
   return (
     <div>
