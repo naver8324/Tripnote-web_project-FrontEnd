1f2857f68933eebed12b7026e1aab982af4ef00b
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-06-07 14:59:43 +0900

    소셜 test

--- src/Hooks/useAxios.js
+++ src/Hooks/useAxios.js
@@ -1,28 +1,44 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
+import api from '../utils/api';
+import { useErrorBoundary } from 'react-error-boundary';
 
-axios.defaults.baseURL = 'http://34.64.39.102:8080';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.timeout = 5000;
-
-const useAxios = ({ method, url, data, shouldFetch }) => {
+const useAxios = ({
+  method,
+  url,
+  data = {},
+  shouldFetch = false,
+  showBoundary = false,
+}) => {
+  const { showBoundary: showBoundaryFunc } = useErrorBoundary();
   const [responseData, setResponseData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const fetchData = async () => {
+  const fetchData = async (
+    params = {},
+    fetchUrl = url,
+    fetchMethod = method,
+  ) => {
     setLoading(true);
     try {
-      const response = await axios.request({
-        method,
-        url: baseURL + url,
-        data,
+      console.log('Requesting:', method, url);
+      console.log('Data:', data);
+      const response = await api.request({
+        method: fetchMethod,
+        url: fetchUrl,
+        ...(fetchMethod === 'DELETE' ? { params: params.params } : { data: params.data || data }),
       });
       setResponseData(response.data);
       setError(null);
+      return response;
     } catch (err) {
-      setError(err.message);
-      setResponseData(null);
+      if (showBoundary) {
+        showBoundaryFunc(err);
+      } else {
+        console.error('Request error:', err);
+        setError(err.message);
+        throw err; // 필요한 경우 호출자에게 에러 전파
+      }
     } finally {
       setLoading(false);
     }
