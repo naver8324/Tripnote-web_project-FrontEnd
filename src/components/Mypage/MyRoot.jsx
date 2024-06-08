bb9b8d1c294f82fb56e1be1ec2734ab64c9d8150
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-06-09 00:53:37 +0900

    성현님꺼 0607 합치고 경로 지역추천 해시태그 생성

--- src/components/Mypage/MyRoot.jsx
+++ src/components/Mypage/MyRoot.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import NaverMap from '../Map/NaverMap';
+import MyPageContent from '../../pages/MyPage/MyPageContent';
 
 const MyRoot = () => {
   return (
     <div>
-      <NaverMap />
+      <MyPageContent />
     </div>
   );
 };
