835a1c82bb8f4f299d9aa09eb3554090b5502e4c
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-06-11 10:40:44 +0900

    401에러 유지 강제로그아웃

--- src/Hooks/user/useLogout.js
+++ src/Hooks/user/useLogout.js
@@ -21,19 +21,13 @@ const useLogout = () => {
       const response = await fetchData();
 
       if (response.status === 200) {
-        // 로그아웃이 성공하면, 로컬 스토리지에서 어세스 토큰 제거
-        localStorage.removeItem('accessToken');
-
         console.log('Logout successful');
-        setIsAuth(false);
-        return true;
       } else {
         console.error('Logout failed:', response.data);
         setError({
           message: '로그아웃에 실패했습니다',
           error: response.data,
         });
-        return false;
       }
     } catch (err) {
       console.error('Error:', err);
@@ -41,8 +35,10 @@ const useLogout = () => {
         message: '로그아웃 중 오류가 발생했습니다',
         error: err.message,
       });
-      return false;
     } finally {
+      // 로그아웃이 성공하든 실패하든, 로컬 스토리지에서 어세스 토큰 제거 및 인증 상태 업데이트
+      localStorage.removeItem('accessToken');
+      setIsAuth(false);
       setLoading(false);
     }
   };
