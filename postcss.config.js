1259c1902a68f18d4550c724842af37f16282c21
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-06-12 11:32:12 +0900

    config 재수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
 };
