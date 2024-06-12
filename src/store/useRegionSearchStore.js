054f1664c559ecff6262dd9c1c1b2c0f8aea3558
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-06-12 19:26:31 +0900

    메인페이지 셀렉터 초기값 경로추천으로 변경 , 경로생성시 데이터 다른루트 가면 데이터 없애기

--- src/store/useRegionSearchStore.js
+++ src/store/useRegionSearchStore.js
@@ -3,7 +3,7 @@ import create from 'zustand';
 const useRegionSearchStore = create((set) => ({
   selectedRegion: 'SEOUL',
   setSelectedRegion: (region) => set({ selectedRegion: region }),
-  redirectPath: '', // 추가
+  redirectPath: '/root/recommend', // 추가
   setRedirectPath: (path) => set({ redirectPath: path }), // 추가
 }));
 
