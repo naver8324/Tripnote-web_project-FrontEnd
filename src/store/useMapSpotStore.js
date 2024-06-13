35de387642e3053dc76cce54b3da9729196edb22
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-06-13 16:41:53 +0900

    경로 스팟중심 추천 경로5개 컬러삽입

--- src/store/useMapSpotStore.js
+++ src/store/useMapSpotStore.js
@@ -5,7 +5,7 @@ const useMapSpotStore = create((set) => ({
   routes: [],
   selectedRouteIndex: null,
   center: { latitude: 37.5665, longitude: 126.978 },
-  polylineColors: ['#F87171', '#6EE7B7', '#93C5FD'],
+  polylineColors: ['#6EE7B7', '#93C5FD', '#c084fc', '#FBBF24', '#F87171'],
   openItems: [],
   setMarkers: (newMarkers) => set({ markers: newMarkers }),
   setRoutes: (newRoutes) => set({ routes: newRoutes }),
