ffef20fa2f4c1c6071faec36fbf41fc52842ff0f
SungHyun Oh <lessbutbttr@gmail.com>
2024-06-13 17:25:48 +0900

    Feat: 댓글 더보기 버튼

--- src/components/Board/Comments.jsx
+++ src/components/Board/Comments.jsx
@@ -5,7 +5,6 @@ import useCommentPost from '../../Hooks/posts/useCommentPost';
 import useGetComment from '../../Hooks/posts/useGetComment';
 import NoData from '../../pages/Board/NoData';
 import { formmateDate } from '../../utils/date';
-import useUserStore from '../../store/useUserStore';
 import useDeleteComment from '../../Hooks/posts/useDeleteComment';
 import useUpdateComment from '../../Hooks/posts/useUpdateComment';
 import useDebounce from '../../Hooks/useDebounce';
@@ -15,35 +14,29 @@ export default function Comments({ postDetail }) {
   const userNickname = localStorage.getItem('userNickname');
   const [comment, setComment] = useState('');
   const { leaveComment, responseData, loading, error } = useCommentPost(id);
-  const {
-    comments,
-    // error: commentErr,
-    // loading: commentLoading,
-    refetch,
-  } = useGetComment(id);
+  const { comments, refetch } = useGetComment(id);
+  const [displayCount, setDisplayCount] = useState(4);
 
-  // 댓글 등록 함수
   const handleComment = async () => {
     if (!comment.length) {
       ToastAlert('내용을 입력해주세요.', 'error');
       return;
     }
     await leaveComment(comment);
-    setComment(''); // 댓글 초기화
+    setComment('');
     ToastAlert('댓글이 등록되었습니다.', 'success');
-    refetch(); // 댓글 목록 불러오기
+    refetch();
   };
 
-  const debounceHandleSave = useDebounce(handleComment, 500)
+  const debounceHandleSave = useDebounce(handleComment, 500);
 
   const deleteComment = useDeleteComment();
-  // 댓글 삭제 함수
+
   const handleDeleteComment = async (commentId) => {
     try {
-      console.log('commentId', commentId);
-      await deleteComment(commentId); // 삭제할 댓글의 ID를 전달하여 호출
+      await deleteComment(commentId);
       ToastAlert('댓글이 삭제되었습니다.', 'success');
-      refetch(); // 삭제 후 댓글 목록 다시 불러오기
+      refetch();
     } catch (error) {
       ToastAlert('댓글 삭제 실패', 'error');
     }
@@ -52,25 +45,33 @@ export default function Comments({ postDetail }) {
   const [updateComment, setUpdateComment] = useState({ id: null, content: '' });
   const { saveComment } = useUpdateComment();
 
-  // 댓글 수정 상태 업데이트 함수
   const handleUpdateComment = (commentData) => {
-    console.log(updateComment);
     setUpdateComment({
       id: commentData.id,
       content: commentData.content,
     });
   };
-  // 댓글 수정 서버 업데이트 함수
+
   const handleSaveUpdateComment = async () => {
     try {
-      await saveComment(updateComment.id ,updateComment.content);
+      await saveComment(updateComment.id, updateComment.content);
       ToastAlert('댓글이 수정되었습니다.', 'success');
-      setUpdateComment({ id: null, content: '' }); // 수정 상태 초기화
-      refetch(); // 수정 후 댓글 목록 다시 불러오기
-
+      setUpdateComment({ id: null, content: '' });
+      refetch();
     } catch (error) {
       ToastAlert('댓글 수정 실패', 'error');
     }
+  };
+
+  const handleKeyDown = (e, saveFunction) => {
+    if (e.key === 'Enter' && !e.shiftKey && !e.metaKey) {
+      e.preventDefault();
+      saveFunction();
+    }
+  };
+
+  const handleShowMore = () => {
+    setDisplayCount((prevCount) => prevCount + 4);
   };
 
   return (
@@ -79,12 +80,7 @@ export default function Comments({ postDetail }) {
         <textarea
           value={comment}
           onChange={(e) => setComment(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault(); // Enter키 누르면 줄바꿈 방지 **Mac OS 체크필요
-              debounceHandleSave(e);
-            }
-          }}
+          onKeyDown={(e) => handleKeyDown(e, debounceHandleSave)}
           placeholder="댓글을 남겨주세요."
           className="pl-5 pt-5 placeholder:opacity-80 resize-none outline-none w-full h-[60px] overflow-auto bg-gray-100 rounded-md pr-[80px]"
         ></textarea>
@@ -98,69 +94,77 @@ export default function Comments({ postDetail }) {
       </div>
       <div>
         {comments && comments.content && comments.content.length > 0 ? (
-          comments.content.map((commentData) => (
-            <div
-              key={commentData.id}
-              className="my-4 p-4 text-sm rounded-md border border-gray-300"
-            >
-              <div className="flex justify-between">
-                <div className="flex gap-3 mb-4 text-title">
-                  <p className="line-clamp-1">{commentData.nickname}</p>
-                  <p className="opacity-70">
-                    {formmateDate(commentData.createdAt)}
-                  </p>
-                </div>
-                {userNickname === commentData.nickname && (
-                  <div className="space-x-2">
-                    <Button
-                      size="small"
-                      className="hover:bg-red-400 hover:text-white"
-                      onClick={() => {
-                        if (updateComment.id === commentData.id) {
-                          handleSaveUpdateComment(); // 저장 함수 호출
-                        } else {
-                          handleUpdateComment(commentData); // 수정 함수 호출
-                        }
-                      }}
-                    >
-                      {updateComment.id === commentData.id
-                        ? '저장'
-                        : '수정'}
-                    </Button>
-                    <Button
-                      size="small"
-                      className="hover:bg-red-400 hover:text-white"
-                      onClick={() => handleDeleteComment(commentData.id)}
-                    >
-                      삭제
-                    </Button>
+          <>
+            {comments.content.slice(0, displayCount).map((commentData) => (
+              <div
+                key={commentData.id}
+                className="my-4 p-4 text-sm rounded-md border border-gray-300"
+              >
+                <div className="flex justify-between">
+                  <div className="flex gap-3 mb-4 text-title">
+                    <p className="line-clamp-1">{commentData.nickname}</p>
+                    <p className="opacity-70">
+                      {formmateDate(commentData.createdAt)}
+                    </p>
                   </div>
+                  {userNickname === commentData.nickname && (
+                    <div className="space-x-2">
+                      <Button
+                        size="small"
+                        className="hover:bg-red-400 hover:text-white"
+                        onClick={() => {
+                          if (updateComment.id === commentData.id) {
+                            handleSaveUpdateComment();
+                          } else {
+                            handleUpdateComment(commentData);
+                          }
+                        }}
+                      >
+                        {updateComment.id === commentData.id ? '저장' : '수정'}
+                      </Button>
+                      <Button
+                        size="small"
+                        className="hover:bg-red-400 hover:text-white"
+                        onClick={() => handleDeleteComment(commentData.id)}
+                      >
+                        삭제
+                      </Button>
+                    </div>
+                  )}
+                </div>
+                {updateComment.id === commentData.id ? (
+                  <textarea
+                    value={updateComment.content}
+                    onChange={(e) =>
+                      setUpdateComment({
+                        ...updateComment,
+                        content: e.target.value,
+                      })
+                    }
+                    onKeyDown={(e) =>
+                      handleKeyDown(e, handleSaveUpdateComment)
+                    }
+                    className="resize-none outline-none w-full h-[60px] overflow-auto bg-gray-100 rounded-md pr-[80px]"
+                  ></textarea>
+                ) : (
+                  <p>{commentData.content}</p>
                 )}
               </div>
-              {updateComment.id === commentData.id ? (
-                <textarea
-                  value={updateComment.content}
-                  onChange={(e) =>
-                    setUpdateComment({
-                      ...updateComment,
-                      content: e.target.value,
-                    })
-                  }
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                      e.preventDefault(); // Enter키 누르면 줄바꿈 방지 **Mac OS 체크필요
-                      handleSaveUpdateComment(e);
-                    }
-                  }}
-                  className="resize-none outline-none w-full h-[60px] overflow-auto bg-gray-100 rounded-md pr-[80px]"
-                ></textarea>
-              ) : (
-                <p>{commentData.content}</p>
-              )}
-            </div>
-          ))
+            ))}
+            {displayCount < comments.content.length && (
+              <div className="text-center mt-4">
+                <Button
+                  onClick={handleShowMore}
+                  size="small"
+                  className="hover:bg-gray-300 text-title px-4"
+                >
+                  더보기
+                </Button>
+              </div>
+            )}
+          </>
         ) : (
-          <></>
+          <NoData message="댓글이 없습니다." />
         )}
       </div>
     </>
