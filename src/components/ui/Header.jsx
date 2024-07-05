539291329bd58de7b6cbf707c8112b121ff889a6
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-07-05 22:10:00 +0900

    header 경로 이동 수정

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -46,15 +46,10 @@ export default function Header() {
   };
 
   const handleCreateClick = () => {
-    setSearchQuery('');
-    setRedirectPath('/root/create'); // Set redirect path for route creation
-    navigate('/');
-    setTimeout(() => {
-      const searchElement = document.getElementById('search');
-      if (searchElement) {
-        searchElement.scrollIntoView({ behavior: 'smooth' });
-      }
-    }, 100);
+    navigate('/root/create');
+  };
+  const handleRecommendClick = () => {
+    navigate('/root/recommend');
   };
 
   return (
@@ -64,7 +59,7 @@ export default function Header() {
           <img className="w-36 h-auto" src={logo} alt="trip note logo" />
         </Link>
         <nav className="flex items-center gap-4 font-medium">
-          <button onClick={handleSearchClick} className="hover:text-prime">
+          <button onClick={handleRecommendClick} className="hover:text-prime">
             경로 추천
           </button>
           <button onClick={handleCreateClick} className="hover:text-prime">
